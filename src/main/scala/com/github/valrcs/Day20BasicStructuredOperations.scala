1e23c4aba1de26ff69485ccff3d0d0cc42beb28b
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-07-25 21:31:12 +0300

    Day 20 SparkUtil

---
+++ src/main/scala/com/github/valrcs/Day20BasicStructuredOperations.scala
@@ -0,0 +1,6 @@
+package com.github.valrcs
+
+object Day20BasicStructuredOperations extends App {
+  println("Chapter 5. Basic Structured Operations")
+  val spark = SparkUtil.getSpark("BasicSpark")
+}
