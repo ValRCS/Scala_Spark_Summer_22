f87b239a0b0fa2916c766bcd940f564cb4ca93a1
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-08-12 10:19:13 +0300

    Day 30 Cube and Pivot

--- src/main/scala/com/github/valrcs/Day29Rollups.scala
+++ src/main/scala/com/github/valrcs/Day29Rollups.scala
@@ -1,7 +1,7 @@
 package com.github.valrcs
 
 import com.github.valrcs.SparkUtil.{getSpark, readDataWithView}
-import org.apache.spark.sql.functions.{col, count, expr, sum, to_date}
+import org.apache.spark.sql.functions.{col, count, expr, grouping_id, sum, to_date}
 
 object Day29Rollups extends App {
   println("Ch7: Rollups")
@@ -47,4 +47,60 @@ object Day29Rollups extends App {
 
   //so this will show sales, quantity and total quantity sold for day by day
   rolledUpDF.where("Country IS NULL").show(50)
+
+  //Cube
+  //A cube takes the rollup to a level deeper. Rather than treating elements hierarchically, a cube
+  //does the same thing across all dimensions. This means that it won’t just go by date over the
+  //entire time period, but also the country. To pose this as a question again, can you make a table
+  //that includes the following?
+  //The total across all dates and countries
+  //The total for each date across all countries
+  //The total for each country on each date
+  //The total for each country across all dates
+  //The method call is quite similar, but instead of calling rollup, we call cube:
+
+  // in Scala
+  dfNoNull.cube("Date", "Country").agg(grouping_id(),sum(col("Quantity")), sum("total"))
+    .select("Date", "Country", "grouping_id()", "sum(Quantity)", "sum(total)").orderBy("Date")
+    .show(50)
+
+  //Grouping Metadata
+  //Sometimes when using cubes and rollups, you want to be able to query the aggregation levels so
+  //that you can easily filter them down accordingly. We can do this by using the grouping_id,
+  //which gives us a column specifying the level of aggregation that we have in our result set. The
+  //query in the example that follows returns four distinct grouping IDs:
+  //Table 7-1. Purpose of grouping IDs
+  //Grouping
+  //ID
+  //Description
+  //3
+  //This will appear for the highest-level aggregation, which will gives us the total quantity
+  //regardless of customerId and stockCode.
+  //2
+  //This will appear for all aggregations of individual stock codes. This gives us the total quantity
+  //per stock code, regardless of customer.
+  //1
+  //This will give us the total quantity on a per-customer basis, regardless of item purchased.
+  //0
+  //This will give us the total quantity for individual customerId and stockCode combinations.
+
+  dfNoNull.cube("customerId", "stockCode").agg(grouping_id(), sum("Quantity"), sum("total"))
+    .orderBy(expr("grouping_id()").desc)
+    .show()
+
+  //Pivot
+  //Pivots make it possible for you to convert a row into a column. For example, in our current data
+  //we have a Country column. With a pivot, we can aggregate according to some function for each
+  //of those given countries and display them in an easy-to-query way:
+  dfWithDate.show(5, false)
+  dfWithDate
+    .select("Country")
+    .distinct()
+    .orderBy("Country")
+    .show(30)
+
+  val pivoted = dfWithDate.groupBy("date").pivot("Country").sum()
+
+  pivoted.show(20, false)
+  //many columns equal to distinc country count * numeric columss in original dfWithDate
 }
