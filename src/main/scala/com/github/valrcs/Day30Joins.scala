9458e060f31d7444cf582bdd701230d570080931
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-08-13 12:02:23 +0300

    Day 30 LEFT SEMI and ANTI JOINS

--- src/main/scala/com/github/valrcs/Day30Joins.scala
+++ src/main/scala/com/github/valrcs/Day30Joins.scala
@@ -5,6 +5,9 @@ import com.github.valrcs.SparkUtil.getSpark
 
 object Day30Joins extends App {
   println("Ch8: Joins")
+  //https://spark.apache.org/docs/latest/sql-ref-syntax-qry-select-join.html
+  //all joins latest syntax
+
   val spark = getSpark("Sparky")
 
   //Join Expressions
@@ -49,7 +52,8 @@ object Day30Joins extends App {
     (1, "Matei Zaharia", 1, Seq(500, 250, 100)),
     (2, "Michael Armbrust", 1, Seq(250, 100)),
     (3, "Valdis Saulespurens", 2, Seq(100,250)),
-    (4, "Victoria Beckham", 42, Seq(100,250)) //42 does not correspond to any school at a time in our graduatePrograms table
+    (4, "Victoria Beckham", 42, Seq(100,250)), //42 does not correspond to any school at a time in our graduatePrograms table
+    (5, "Harry Potter", 77, Seq(100,250,500,1000)), //42 does not correspond to any school at a time in our graduatePrograms table
   )
     .toDF("id", "name", "graduate_program", "spark_status")
 
@@ -58,14 +62,15 @@ object Day30Joins extends App {
     (2, "Masters", "EECS", "UC Berkeley"),
     (1, "Ph.D.", "EECS", "UC Berkeley"),
     (3, "Masters", "EECS", "University of Latvia"),
+    (4, "Masters", "EECS", "Vilnius University"),
   )
     .toDF("id", "degree", "department", "school")
 
   val sparkStatus = Seq(
-    (500, "Vice President"),
-    (250, "PMC Member"),
-    (100, "Contributor"))
-    .toDF("id", "status")
+    (500, "Vice President", 500_000),
+    (250, "PMC Member", 15_000),
+    (100, "Contributor", 10))
+    .toDF("id", "status", "salary")
 
   person.show()
   graduateProgram.show()
@@ -113,6 +118,8 @@ object Day30Joins extends App {
   //together) the rows that evaluate to true or false. If there is no equivalent row in either the left or
   //right DataFrame, Spark will insert null
 
+  println("FULL OUTER JOIN")
+
   person.join(graduateProgram, joinExpression, "outer").show()
 
   spark.sql(
@@ -124,5 +131,94 @@ object Day30Joins extends App {
     .show()
 
   //so we should see some null values in an FULL OUTER JOIN
+
+  //Left Outer Joins
+  //Left outer joins evaluate the keys in both of the DataFrames or tables and includes all rows from
+  //the left DataFrame as well as any rows in the right DataFrame that have a match in the left
+  //DataFrame. If there is no equivalent row in the right DataFrame, Spark will insert null
+
+  println("LEFT OUTER JOINS")
+  person.join(graduateProgram, joinExpression, "left_outer").show()
+  spark.sql(
+    """
+      |SELECT * FROM person
+      |LEFT OUTER JOIN graduateProgram
+      |ON person.graduate_program = graduateProgram.id
+      |""".stripMargin)
+    .show()
+
+  //Right Outer Joins
+  //Right outer joins evaluate the keys in both of the DataFrames or tables and includes all rows
+  //from the right DataFrame as well as any rows in the left DataFrame that have a match in the right
+  //DataFrame. If there is no equivalent row in the left DataFrame, Spark will insert null:
+
+  println("RIGHT OUTER JOIN")
+
+  person.join(graduateProgram, joinExpression, "right_outer").show()
+  spark.sql(
+    """
+      |SELECT * FROM person
+      |RIGHT OUTER JOIN graduateProgram
+      |ON person.graduate_program = graduateProgram.id
+      |""".stripMargin)
+    .show()
+
+
+  //Left Semi Joins
+  //Semi joins are a bit of a departure from the other joins. They do not actually include any values
+  //from the right DataFrame. They only compare values to see if the value exists in the second
+  //DataFrame. If the value does exist, those rows will be kept in the result, even if there are
+  //duplicate keys in the left DataFrame. Think of left semi joins as filters on a DataFrame, as
+  //opposed to the function of a conventional join:
+
+  println("LEFT SEMI JOIN")
+  //notice we are using the same joinExpression but we start with graduateProgram here
+  graduateProgram.join(person, joinExpression, "left_semi").show() //should show only programs with graduates
+  person.join(graduateProgram, joinExpression, "left_semi").show() //should show only persons who attended a known university
+
+
+  //we create a new dataFrame by using Union of two dataframes
+  // in Scala
+  //in this case the id is a duplicate of previous one
+  //often you do not want this :)
+  val gradProgram2 = graduateProgram.union(Seq(
+    (0, "Masters", "Duplicated Row", "Duplicated School")).toDF())
+  gradProgram2.createOrReplaceTempView("gradProgram2")
+
+  gradProgram2.join(person, joinExpression, "left_semi").show()
+
+  spark.sql(
+    """
+      |SELECT * FROM gradProgram2 LEFT SEMI JOIN person
+      |ON gradProgram2.id = person.graduate_program
+      |""".stripMargin)
+    .show()
+
+  // more on differences on INNER JOIN and LEFT SEMI JOIN
+  //https://stackoverflow.com/questions/21738784/difference-between-inner-join-and-left-semi-join
+
+  //Left Anti Joins
+  //Left anti joins are the opposite of left semi joins. Like left semi joins, they do not actually
+  //include any values from the right DataFrame. They only compare values to see if the value exists
+  //in the second DataFrame. However, rather than keeping the values that exist in the second
+  //DataFrame, they keep only the values that do not have a corresponding key in the second
+  //DataFrame. Think of anti joins as a NOT IN SQL-style filter:
+
+  println("LEFT ANTI JOIN")
+
+  graduateProgram.join(person, joinExpression, "left_anti").show()
+
+  spark.sql(
+    """
+      |SELECT * FROM graduateProgram
+      |LEFT ANTI JOIN person
+      |ON graduateProgram.id = person.graduate_program
+      |""".stripMargin)
+    .show()
+
+  //so we should expect to see University of Latvia and Vilnius University since they do not have any known students here
+
+  //similarly we should expect to see students without a matching university program if our left table is persons
+  person.join(graduateProgram, joinExpression, "left_anti").show()
 
 }
