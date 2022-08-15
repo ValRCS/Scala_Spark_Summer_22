25c699dcce37eb4c33bcfcc0bec601d5993dc1f7
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-08-15 21:47:33 +0300

    Day 31 Writing SQLite data

--- src/main/scala/com/github/valrcs/Day31SQLite.scala
+++ src/main/scala/com/github/valrcs/Day31SQLite.scala
@@ -55,5 +55,56 @@ object Day31SQLite extends App {
   dbDataFrame.describe().show()
   dbDataFrame.show(5, false)
 
+  //As we create this DataFrame, it is no different from any other: you can query it, transform it, and
+  //join it without issue. You’ll also notice that there is already a schema, as well. That’s because
+  //Spark gathers this information from the table itself and maps the types to Spark data types. Let’s
+  //get only the distinct locations to verify that we can query it as expected:
+
+  dbDataFrame.select("DEST_COUNTRY_NAME").distinct().show(5)
+
+  //this is optional and could be faster - basically you would ask the database SQL engine to do more work
+  //and spark engine to do less
+
+  //Spark can’t translate all of its own functions into the functions available in the SQL database in
+  //which you’re working. Therefore, sometimes you’re going to want to pass an entire query into
+  //your SQL that will return the results as a DataFrame. Now, this might seem like it’s a bit
+  //complicated, but it’s actually quite straightforward. Rather than specifying a table name, you just
+  //specify a SQL query. Of course, you do need to specify this in a special way; you must wrap the
+  //query in parenthesis and rename it to something—in this case, I just gave it the same table name:
+
+  // in Scala
+  val pushdownQuery = """(SELECT DISTINCT(DEST_COUNTRY_NAME) FROM flight_info)
+AS flight_info"""
+  val dbDataFrameFromQuery = spark.read.format("jdbc")
+    .option("url", url).option("dbtable", pushdownQuery).option("driver", driver)
+    .load()
+
+  //Now when you query this table, you’ll actually be querying the results of that query. We can see
+  //this in the explain plan. Spark doesn’t even know about the actual schema of the table, just the
+  //one that results from our previous query:
+
+  println(dbDataFrameFromQuery.explain())
+
+  //Writing to SQL Databases
+  //Writing out to SQL databases is just as easy as before. You simply specify the URI and write out
+  //the data according to the specified write mode that you want. In the following example, we
+  //specify overwrite, which overwrites the entire table. We’ll use the CSV DataFrame that we
+  //defined earlier in order to do this:
+
+  dbDataFrameFromQuery.show(10)
+
+  // in Scala
+  val newPath = "jdbc:sqlite:src/resources/tmp/my-sqlite.db"
+
+  val props = new java.util.Properties
+  props.setProperty("driver", "org.sqlite.JDBC")
+
+  dbDataFrameFromQuery //before write you could filter do some aggregartion etc
+    .write
+    .mode("overwrite")
+    .jdbc(newPath, tablename, props)
+
+  //TODO how would you add extra tables to already existing SQL database - probably involves append
+
 
 }
